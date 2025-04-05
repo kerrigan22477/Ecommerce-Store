20a463eb72c61ea0c4faae1c380a78ae125e71ee
kerrigan22477 <k3rr16an@gmail.com>
2025-04-05 10:12:38 -0700

    init commit

---
+++ ecom_site/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBarComponent from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <NavBarComponent>
+
+    </NavBarComponent>
+  );
+}
+
+export default App;
---
+++ ecom_site/src/components/navbar.js
@@ -0,0 +1,17 @@
+import {Button, Container, Modal, Navbar} from 'react-bootstrap'
+
+function NavBarComponent() {
+
+    return (
+        <Navbar expand='sm'>
+            <Navbar.Brand href="/">Ecom Store</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Button>Cart 0 Items</Button>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+
+}
+
+export default NavBarComponent;
